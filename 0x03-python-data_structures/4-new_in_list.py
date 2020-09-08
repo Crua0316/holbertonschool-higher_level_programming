9637e5d97e0b0456ea5b0e9c66991c36b8a4d522
Crua0316 <cresrugi@gmail.com>
2020-09-08 18:35:23 +0000

    Fix task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
+    new_list = my_list[:]
     a = len(my_list)
     if idx < 0:
         return new_list
-    elif idx > a and idx != range(a):
+    elif idx >= a and idx != range(a):
         return new_list
     else:
         new_list[idx] = element
