97e690a6035208a02ed3107e19a3ea3c169054f2
Crua0316 <cresrugi@gmail.com>
2020-09-08 18:28:53 +0000

    fix output to taks 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,9 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     a = len(my_list)
     if idx < 0:
-        return my_list
+        return new_list
     elif idx > a and idx != range(a):
-        return my_list
+        return new_list
     else:
         new_list[idx] = element
         return new_list
