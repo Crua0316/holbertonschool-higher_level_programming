fea1313ad0621a8026e8d3c7415519655049f7fb
Crua0316 <cresrugi@gmail.com>
2020-09-29 01:09:41 +0000

    Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -72,5 +72,5 @@ class Rectangle:
         """Return string of rectangles with #"""
         if self.height == 0 or self.height == 0:
             return ""
-        rec = "#" * self.width
-        return "\n".join(list(rec for a in range(self.height)))
+        rec = '#' * self.width
+        return '\n'.join(list(rec for a in range(self.height)))
