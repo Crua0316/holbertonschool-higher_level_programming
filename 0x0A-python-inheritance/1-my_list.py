3597c68b84977f82a337d4bd68b1b3ce4e119a69
Crua0316 <cresrugi@gmail.com>
2020-10-07 01:31:13 +0000

    Try fix task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""class list that inherits from list"""
+"""
+class list that inherits from list
+"""
 
 
 class MyList(list):
-    """Subclass of list"""
+    """
+    Subclass of list
+    """
     def __init__(self):
-        """Initializace all instance of method"""
+        """
+        Initializace all instance of method
+        """
         super().__init__
 
     def print_sorted(self):
-        """method to print my list ascending sort"""
+        """
+        method to print my list ascending sort
+        """
         print(sorted(self))
