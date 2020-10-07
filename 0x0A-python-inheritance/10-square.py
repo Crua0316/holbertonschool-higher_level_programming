b6722bf8eec803ba252bf011131b4438fea93acf
Crua0316 <cresrugi@gmail.com>
2020-10-07 01:59:46 +0000

    Fix task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Module that define a class Rectangle
+"""
+Subclass Rectangle
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class square that inherits from Rectangle
-    """
+    """Square class"""
     def __init__(self, size):
         """Init with super function to use all attributes from parent class
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
+        """Returns the area"""
         return self.__size * self.__size
-        
