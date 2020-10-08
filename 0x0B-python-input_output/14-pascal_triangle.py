b8266077d2c81d3c97c9ce051f57d6436863da30
Crua0316 <cresrugi@gmail.com>
2020-10-08 01:48:14 +0000

    Fixed task 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""pascal tringle in python
-"""
-
-
 def pascal_triangle(n):
+    """creatign pascals triangle
+    Args:
+        n: input number of height of triangle
+    Return:
+        list of tirangle or empty list when n is lower and equal to 0
     """
-    implementation
-    """
-    new = []
     if n <= 0:
-        return new
-    for i in range(n):
-        if i == 0:
-            new.append([1])
-        elif i == 1:
-            new.append([1, 1])
-        else:
-            new_2 = []
-            for j in range(i + 1):
-                new_2.append(j)
-                print(new_2)
-            for j in range(i):
-                new_2[0], new_2[i] = 1, 1
-                new_2[j] = new[i - 1][j] + new[i - 1][j - 1]
-            new.append(new_2)
+        return list()
+    new = [[1]]
+    for i in range(1, n):
+        tmp = [1]
+        for j in range(1, i):
+            tmp.append(new[i - 1][j - 1] + new[i - 1][j])
+        tmp.append(1)
+        new.append(tmp)
     return new
