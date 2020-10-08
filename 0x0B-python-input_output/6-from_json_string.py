2e06bf2afc1ddebd423853ca268d68c5ad57422f
Crua0316 <cresrugi@gmail.com>
2020-10-08 01:18:35 +0000

    Task 6

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a function that returns an object (Python data structure)
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Args:
+        My_str (str): [object represented by JSON]
+    Returns:
+    [object]
+    """
+    return json.loads(my_str)
