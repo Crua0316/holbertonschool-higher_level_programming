f758e0132ebfa7f36f5c9093428fe40bd0982f23
crua0316 <cresrugi@gmail.com>
2020-12-03 00:48:06 -0500

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+square method - print square
+"""
+
+
+class Square:
+    """
+    print a square class
+    """
+    def __init__(self, size):
+        """
+        define size value
+        """
+        self.__size = size
