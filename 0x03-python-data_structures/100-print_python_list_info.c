45f9c1a2b1c7ec98da275f771943148cf6b30e28
crua0316 <cresrugi@gmail.com>
2020-12-18 14:08:21 -0500

    Try fix task advanced

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,19 @@
+  #include <stdio.h>
 #include <stdlib.h>
-#include <Python.h">
+#include <Python.h>
+/**
+ * print_python_list_info - print python lists
+ * @p: pyobject.
+ * Return: Nothing.
+ */
 void print_python_list_info(PyObject *p)
 {
-    unsigned int i = 0;
-    printf("[*] Size of the Python List = %ld", pyList_Size(p));
-    printf("[*] Allocated = %ld\n", ((PyListObject*)(p))->allocated);
-    for(i = 0; i < PySize; i++)
-    {
-        printf("Element %i: %s\n", i, (PyType(PyList_Getitem(p, i))->tp_name));
-    }
+	int i = 0;
+
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)(p))->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
