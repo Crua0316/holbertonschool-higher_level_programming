67683bcdb026d4a1acc559c127dabce191cb4af4
crua0316 <cresrugi@gmail.com>
2020-12-18 13:44:05 -0500

    Update task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
+
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list2 = my_list.copy()
+    n_list = my_list.copy()
     for n, i in enumerate(my_list):
         if i == search:
-            my_list2[n] = replace
-    return(my_list2)
+            n_list[n] = replace
+    return n_list
