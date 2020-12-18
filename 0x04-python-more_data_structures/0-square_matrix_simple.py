68efebfa9bd4584fe3f4cba7592c64b4448470f4
crua0316 <cresrugi@gmail.com>
2020-12-18 13:41:13 -0500

    Try fix task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix2 = []
-    for i in matrix:
-        for j in i:
-            matrix2.append(j*j)
-    return(matrix2)
+    return [list(map((lambda x: x ** 2), i)) for i in matrix]
